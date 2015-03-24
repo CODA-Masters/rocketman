de724d3d62dc30ad74480e7588d7d3a4bb10eb95
julioxus <julioxus@gmail.com>
2015-03-24 13:32:11 +0100

    El trabajo de esta mañana

---
+++ Assets/UniverseScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UniverseScript : MonoBehaviour {
+	public GameObject Bar;
+
+	// Use this for initialization
+	void Start () {
+		Bar = GameObject.Find ("DriveBar");
+		Bar.GetComponent< GUIBarScript > ().ScaleSize = 0;
+		Bar.GetComponent< GUIBarScript > ().DisplayText = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
