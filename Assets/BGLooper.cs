19e77328898ece881a8f74463cb48a192eca8d8b
julioxus <julioxus@gmail.com>
2015-03-31 19:57:37 +0200

    Looping del escenario conseguido

---
+++ Assets/BGLooper.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGLooper : MonoBehaviour {
+
+	int numBGPanels = 4;
+
+	void OnTriggerEnter2D(Collider2D collider){
+		float widthOfBGObject = ((BoxCollider2D)collider).size.x*1.5f;
+
+		Vector3 pos = collider.transform.position;
+		pos.x += (widthOfBGObject * numBGPanels);
+		collider.transform.position = pos;
+	}
+}
