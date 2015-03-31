e3e0210d89d1a5dacf6ad7bee8524a2290fdb3db
enpi <juammanc@hotmail.com>
2015-03-31 17:43:48 +0200

    Inicialización aleatoria en altura de las plataformas

---
+++ Assets/PlatformController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformController : MonoBehaviour {
+
+	public GameObject platform1, platform2, platform3, platform4; 
+
+	public float MaxPlatformHeight;
+	
+	public float MinPlatformHeight;
+
+	private float RandomYDistance; 
+	
+	// Use this for initialization
+	
+	void Start () 
+		
+	{
+
+		platform1 = GameObject.FindGameObjectWithTag ("Plataforma1");
+		platform2 = GameObject.FindGameObjectWithTag ("Plataforma2");
+		platform3 = GameObject.FindGameObjectWithTag ("Plataforma3");
+		platform4 = GameObject.FindGameObjectWithTag ("Plataforma4");
+
+		
+		RandomYDistance = Random.Range(MinPlatformHeight, MaxPlatformHeight);
+		platform1.transform.Translate(0, RandomYDistance, 0);
+
+		RandomYDistance = Random.Range(MinPlatformHeight, MaxPlatformHeight);
+		platform2.transform.Translate(0, RandomYDistance, 0);
+
+		RandomYDistance = Random.Range(MinPlatformHeight, MaxPlatformHeight);
+		platform3.transform.Translate(0, RandomYDistance, 0);
+
+		RandomYDistance = Random.Range(MinPlatformHeight, MaxPlatformHeight);
+		platform4.transform.Translate(0, RandomYDistance, 0);	
+		
+	}
+
+
+	void Update () 
+		
+	{ 
+
+		
+	}
+
+}
