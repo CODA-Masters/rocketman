2f137766a0e42c1b74b2f2c96b7ddab09fd936ad
julioxus <julioxus@gmail.com>
2015-03-31 22:04:03 +0200

    Generación de mapa aleatoria con problemo

--- Assets/BGLooper.cs
+++ Assets/BGLooper.cs
@@ -4,12 +4,18 @@ using System.Collections;
 public class BGLooper : MonoBehaviour {
 
 	int numBGPanels = 4;
+	int numPlatforms = 8;
 
 	void OnTriggerEnter2D(Collider2D collider){
-		float widthOfBGObject = ((BoxCollider2D)collider).size.x*1.5f;
 
 		Vector3 pos = collider.transform.position;
-		pos.x += (widthOfBGObject * numBGPanels);
+		if (collider.tag == "Platform") {
+			float widthOfBGObject = ((BoxCollider2D)collider).size.x*2f;
+			pos.x += (widthOfBGObject * numPlatforms);
+		} else {
+			float widthOfBGObject = ((BoxCollider2D)collider).size.x*1.5f;
+			pos.x += (widthOfBGObject * numBGPanels);
+		}
 		collider.transform.position = pos;
 	}
 }
--- Assets/PlatformController.cs
+++ Assets/PlatformController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class PlatformController : MonoBehaviour {
 
-	public GameObject platform1, platform2, platform3, platform4, player; 
+	public GameObject platform1, platform2, platform3, platform4, platform5, platform6, platform7, platform8, player; 
 
 	public float MaxPlatformHeight;
 	
@@ -17,10 +17,14 @@ public class PlatformController : MonoBehaviour {
 		
 	{
 
-		platform1 = GameObject.FindGameObjectWithTag ("Plataforma1");
-		platform2 = GameObject.FindGameObjectWithTag ("Plataforma2");
-		platform3 = GameObject.FindGameObjectWithTag ("Plataforma3");
-		platform4 = GameObject.FindGameObjectWithTag ("Plataforma4");
+		platform1 = GameObject.Find("platform1");
+		platform2 = GameObject.Find("platform2");
+		platform3 = GameObject.Find("platform3");
+		platform4 = GameObject.Find("platform4");
+		platform5 = GameObject.Find("platform5");
+		platform6 = GameObject.Find("platform6");
+		platform7 = GameObject.Find("platform7");
+		platform8 = GameObject.Find("platform8");
 
 		player = GameObject.FindGameObjectWithTag ("Player");
 
@@ -36,7 +40,19 @@ public class PlatformController : MonoBehaviour {
 		platform3.transform.Translate(0, RandomYDistance, 0);
 
 		RandomYDistance = Random.Range(MinPlatformHeight, MaxPlatformHeight);
-		platform4.transform.Translate(0, RandomYDistance, 0);	
+		platform4.transform.Translate(0, RandomYDistance, 0);
+
+		RandomYDistance = Random.Range(MinPlatformHeight, MaxPlatformHeight);
+		platform5.transform.Translate(0, RandomYDistance, 0);	
+
+		RandomYDistance = Random.Range(MinPlatformHeight, MaxPlatformHeight);
+		platform6.transform.Translate(0, RandomYDistance, 0);	
+
+		RandomYDistance = Random.Range(MinPlatformHeight, MaxPlatformHeight);
+		platform7.transform.Translate(0, RandomYDistance, 0);	
+
+		RandomYDistance = Random.Range(MinPlatformHeight, MaxPlatformHeight);
+		platform8.transform.Translate(0, RandomYDistance, 0);	
 		
 	}
 
