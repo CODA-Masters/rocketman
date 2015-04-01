633c649a4e2f9f09047df721f16e7cceb6024636
julioxus <julioxus@gmail.com>
2015-04-01 21:57:50 +0200

    Arreglado scroll del fondo. Fallan las plataformas 3 y 7 al desplazar

--- Assets/BGLooper.cs
+++ Assets/BGLooper.cs
@@ -2,21 +2,21 @@
 using System.Collections;
 
 public class BGLooper : MonoBehaviour {
-
+	
 	int numBGPanels = 4;
 	int numPlatforms = 8;
-
+	
 	void OnTriggerEnter2D(Collider2D collider){
-
+		
 		Vector3 pos = collider.transform.position;
+		float widthOfBGObject = 0;
 		if (collider.tag == "Platform") {
-			float widthOfBGObject = ((BoxCollider2D)collider).bounds.size.x;
+			widthOfBGObject = ((BoxCollider2D)collider).size.x*2f;
 			pos.x += (widthOfBGObject * numPlatforms);
 		} else {
-			float widthOfBGObject = ((BoxCollider2D)collider).bounds.size.x;
-			pos.x += (widthOfBGObject * numPlatforms);
+			widthOfBGObject = ((BoxCollider2D)collider).size.x*1.655f;
+			pos.x += (widthOfBGObject * numBGPanels);
 		}
 		collider.transform.position = pos;
-		//collider.attachedRigidbody.transform.position = pos;
 	}
 }
