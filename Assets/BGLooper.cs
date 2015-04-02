1fe090cb2ef3300c63e729d40e2be90d43dfead1
julioxus <julioxus@gmail.com>
2015-04-02 13:07:08 +0200

    Generació

--- Assets/BGLooper.cs
+++ Assets/BGLooper.cs
@@ -5,6 +5,10 @@ public class BGLooper : MonoBehaviour {
 	
 	int numBGPanels = 4;
 	int numPlatforms = 8;
+	private float RandomYDistance; 
+	public float MaxPlatformHeight;
+	public float MinPlatformHeight;
+
 	
 	void OnTriggerEnter2D(Collider2D collider){
 		
@@ -13,7 +17,9 @@ public class BGLooper : MonoBehaviour {
 		if (collider.tag == "Platform") {
 			widthOfBGObject = ((BoxCollider2D)collider).size.x*2f;
 			pos.x += (widthOfBGObject * numPlatforms);
-		} else {
+			RandomYDistance = Random.Range(MinPlatformHeight, MaxPlatformHeight);
+			collider.transform.Translate(0, RandomYDistance, 0);
+		} else if (collider.tag == "Background"){
 			widthOfBGObject = ((BoxCollider2D)collider).size.x*1.655f;
 			pos.x += (widthOfBGObject * numBGPanels);
 		}
