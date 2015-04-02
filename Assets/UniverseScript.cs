4e280c21aecafb6fbf9925bb6713ae1002eef2e0
julioxus <julioxus@gmail.com>
2015-04-02 13:43:32 +0200

    Arreglando problemas del fondo

--- Assets/UniverseScript.cs
+++ Assets/UniverseScript.cs
@@ -2,17 +2,21 @@
 using System.Collections;
 
 public class UniverseScript : MonoBehaviour {
-	public GameObject Bar;
+	GameObject Bar;
+	GameObject Player;
+	GameObject Background;
 
 	// Use this for initialization
 	void Start () {
 		Bar = GameObject.Find ("DriveBar");
+		Player = GameObject.FindGameObjectWithTag ("Player");
+		Background = GameObject.FindGameObjectWithTag ("Background");
 		Bar.GetComponent< GUIBarScript > ().ScaleSize = 0;
 		Bar.GetComponent< GUIBarScript > ().DisplayText = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
