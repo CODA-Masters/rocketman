096cf2870e41a0d7f42e6e2a460c70ac571aee9b
julioxus <julioxus@gmail.com>
2015-04-04 03:21:14 +0200

    Agujero loco que te absorbe

---
+++ Assets/tragabicho.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tragabicho : MonoBehaviour {
+
+	GameObject Player;
+	bool contact = false;
+	
+	void Start(){
+		Player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	void OnTriggerEnter2D(Collider2D collider){
+		contact = true;
+		Player.GetComponent<Rigidbody2D>().gravityScale = 0;
+		Player.GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+	}
+	
+	void Update(){
+
+	}
+}
